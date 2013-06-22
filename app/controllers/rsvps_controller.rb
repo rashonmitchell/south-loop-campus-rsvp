16fa907628c85a9f7a7dde50a158e0e3259c70fc
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-06-21 22:53:12 -0500

    Exports

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,5 +1,6 @@
 class RsvpsController < ApplicationController
   before_filter :authenticate, only: :index
+  before_filter :set_locale
 
   def create
     Rsvp.create!(params.slice(:name_one, :name_two, :name_three, :bringing))
@@ -8,6 +9,11 @@ class RsvpsController < ApplicationController
 
   def index
     @rsvps = Rsvp.all
+    respond_to do |format|
+      format.html {render "index" }
+      format.csv { send_data @rsvps.to_csv }
+      format.xls  { send_data @rsvps.to_csv(col_sep: "\t") }
+    end
   end
 
   protected
@@ -16,5 +22,9 @@ class RsvpsController < ApplicationController
     authenticate_or_request_with_http_basic do |username, password|
       username == "foo" && password == "bar"
     end
+  end
+
+  def set_locale
+    I18n.locale = :en
   end
 end
